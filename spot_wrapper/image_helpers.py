6465e55b47104e54a30f13cd754a0c1bb8ad3b16
Ayush Agrawal <72944387+Ayush8120@users.noreply.github.com>
2025-01-20 08:11:03 +0800

    walkObject

---
+++ spot_wrapper/image_helpers.py
@@ -0,0 +1,18 @@
+import cv2
+
+def cv_mouse_callback(event, x, y, flags, param):
+    global g_image_click, g_image_display
+    clone = g_image_display.copy()
+    if event == cv2.EVENT_LBUTTONUP:
+        g_image_click = (x, y)
+    else:
+        # Draw some lines on the image.
+        #print('mouse', x, y)
+        color = (30, 30, 30)
+        thickness = 2
+        image_title = 'Click to walk up to something'
+        height = clone.shape[0]
+        width = clone.shape[1]
+        cv2.line(clone, (0, y), (width, y), color, thickness)
+        cv2.line(clone, (x, 0), (x, height), color, thickness)
+        cv2.imshow(image_title, clone)
